f5be02b29fc8a6ef4fb34b8417a261b6bb6db96f
mayerll <mayerlbp@hotmail.com>
2026-03-16 16:46:11 -0700

    Update 02-deploy-supabase.sh

--- 02-deploy-supabase.sh
+++ 02-deploy-supabase.sh
@@ -46,6 +46,8 @@ echo "Waiting for ExternalSecrets CRDs..."
 kubectl wait --for=condition=established --timeout=60s crd/secretstores.external-secrets.io
 kubectl wait --for=condition=established --timeout=60s crd/externalsecrets.external-secrets.io
 
+sleep 600
+
 # --- 4. Apply AWS SecretStore / ExternalSecrets ---
 echo "Step 4: Apply SecretStore and ExternalSecrets"
 kubectl apply -f kubernetes/operators/aws-secret-store.yaml
